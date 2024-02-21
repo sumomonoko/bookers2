0c0442c5ff8a54382241593740f466ca64b8d043
EC2 Default User <ec2-user@ip-172-31-9-69.ap-northeast-1.compute.internal>
2024-02-21 13:12:30 +0000

    [Update] 3-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+　#def after_sign_in_path_for(resource)
+    #[name]_path
+  #end
+
+  #def after_sign_out_path_for(resource)
+    #[name]_path
+  #end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- db/migrate/20240219115427_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.1]
-  def change
-    create_table :users do |t|
-      t.text :name
-      t.text :introduction
-      t.timestamps
-    end
-  end
-end
